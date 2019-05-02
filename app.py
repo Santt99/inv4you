cb07fe5d9248b86af37de484cbff2aa04fcd763e
Santiago <santiago10.jbch@gmail.com>
2019-05-01 21:16:11 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('./auth/login.html')
+
+@app.route('/home', methods=["POST"])
+def home():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    print(password)
+    return 'home'
+
+@app.route('/register')
+def register():
+    return 'Register'
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
