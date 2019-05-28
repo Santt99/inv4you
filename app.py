a211493cdcfbdbc719cd3b643a3a7cfcbca98243
Santiago <santiago10.jbch@gmail.com>
2019-05-28 00:54:12 -0500

    csv upload

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-
+import os
 from flask import Flask, render_template, request, redirect,url_for, flash, session
 from db import Database
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 # settings
 app.secret_key = "mysecretkey"
+UPLOAD_FOLDER = '/home/s3xy/Documents/university/plugins/inv4you/invs/'
+ALLOWED_EXTENSIONS = set(['csv'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 
 @app.route('/')
 def Index():
@@ -70,7 +81,6 @@ def saveNewInventory():
 
 @app.route('/register', methods=["POST", "GET"])
 def register():
-    
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
@@ -84,7 +94,28 @@ def register():
             return redirect(url_for('Index'))
     elif request.method == 'GET':
         return render_template('./auth/register.html')
-        
+
+
+@app.route('/uploadInv', methods=["POST", "GET"])
+def uploadInv():
+    if 'id' in session:
+        if request.method == 'POST':
+                if request.files :
+                    file = request.files["inv"]
+                    file.save(os.path.join(app.config["UPLOAD_FOLDER"],file.filename))
+                    print(file.filename + " saved!")
+                    des = request.form.get('description')
+                    response = db.createInventory(session['id'], file.filename, des)
+                    if response:
+                        flash('User Logged Successfully')
+                        return redirect(url_for('Home'))
+                    else:
+                        return "Fail"
+        elif request.method == 'GET':        
+            flash('User Logged Successfully')
+            return render_template('uploadInventory.html')
+    else:
+        redirect(url_for(Index))
 
 @app.route('/styles.css')
 def sendStyles():
